af5df35082b529ae836090184eb051c051a0c097
Bianca Dias Barbosa <biadiasbarbosa@hotmail.com>
2025-06-25 17:14:32 -0700

    #63 Math - Easy

---
+++ Math/Easy/item2235.js
@@ -0,0 +1,11 @@
+/* 
+- Name: 2235. Add Two Integers
+- Description: Given two integers num1 and num2, return the sum of the two integers.
+- My line of thought: Basic addition of two integers is straightforward. I can simply return the sum of the two numbers.
+- Time complexity: O(1)
+- Space complexity: O(1)
+*/
+
+function sum (num1, num2) {
+    return num1 + num2;
+}
